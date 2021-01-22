f6f02e8b3d215e757606459461a8d0f89d524c12
LIN <jimmylinh@icloud.com>
2021-01-22 02:32:30 -0800

    pressure_test modified

--- latest-server/pressure_test.py
+++ latest-server/pressure_test.py
@@ -9,22 +9,32 @@ from datetime import date, datetime
 * uploadBuffer += (Date().GetTime+",\(velocity),\(accCal[1]),\(gyroCal[2]),\(latitude),\(longitude),\n")
 '''
 
-records_num = 0
-url = 'http://59.78.28.3:5000/register'
+records_num = 3000
+url = 'http://server.acemap.cn:24050'
 
-short_data = datetime.utcnow().isoformat("T") + "Z" + \
+
+def short_data():
+    return datetime.utcnow().isoformat("T") + "Z" + \
              ',1.0,1.0,0.1,120,30,\n'
+
+
 long_data = ''
 for i in range(records_num):
-    long_data += short_data
+    long_data += short_data()
 
 json_body = {
     "UserID": 1,
     "LaunchTimestamp": datetime.utcnow().isoformat("T"),
     "Data": long_data
 }
-
-r = requests.post(url=url, json=json_body)
+# register
+# r = requests.post(url=url+'/register', auth=('mike', '123'))
+# login
+# r = requests.post(url=url+'/login', auth=('mike', '123'))
+#upload
+access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwdWJsaWNfaWQiOiI3MDIzNWMwNi1jNzYxLTQzYzctYTA2My02NjQwMjA1YmVjYTEiLCJleHAiOjE2NDI2NTEzMTV9.s5iYigksac-loZSokLoTy1GjaSWJwB_6Ctnv8ohqmdQ"
+r = requests.post(url=url+'/upload', json=json_body,
+                  headers={'x-access-tokens': access_token})
 print(r.headers)
 print(r.status_code)
 print(r.text)
