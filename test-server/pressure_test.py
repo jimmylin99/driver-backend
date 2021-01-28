8f70a6c30de753f487465f7f55bcb2f66967759f
LIN <jimmylinh@icloud.com>
2021-01-27 23:14:53 -0800

    remove latest-server directory

--- test-server/pressure_test.py
+++ test-server/pressure_test.py
@@ -9,9 +9,9 @@ from datetime import date, datetime
 * uploadBuffer += (Date().GetTime+",\(velocity),\(accCal[1]),\(gyroCal[2]),\(latitude),\(longitude),\n")
 '''
 
-records_num = 1500
-# url = 'http://server.acemap.cn:24050'
-url = 'http://localhost:5001'
+records_num = 1
+url = 'http://server.acemap.cn:24050'
+# url = 'http://localhost:5001'
 
 
 def short_data():
@@ -25,7 +25,7 @@ for i in range(records_num):
 
 json_body = {
     "UserID": 1,
-    "LaunchTimestamp": datetime.utcnow().isoformat("T"),
+    "LaunchTimestamp": datetime.utcnow().isoformat("T") + "Z",
     "Data": long_data
 }
 # register
@@ -33,7 +33,7 @@ json_body = {
 # login
 # r = requests.post(url=url+'/login', auth=('mike2', '123'))
 #upload
-access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Im1pa2UyIiwiZXhwIjoxNjQyODY3Mzc1fQ.PBYbxbDAcoQHRLRey2CahEYn3YWQUX3kPGgGgAFZ0B4"
+access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Im1pa2UyIiwiZXhwIjoxNjQyOTE5NjA4fQ.J-yczjSd2ZiVjQnRV6jXxw6DeBLpd5RiVeywsuz1aAs"
 from time import perf_counter
 T1 = perf_counter()
 r = requests.post(url=url+'/upload', json=json_body,
